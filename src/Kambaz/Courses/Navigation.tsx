02cab34595fc42e3fba7fb3e1b973e9e2e60c854
yatish79 <wutla.y@northeastern.edu>
2025-02-05 18:52:09 -0500

    navigation update

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "../styles.css";
+
 export default function CoursesNavigation() {
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
@@ -7,10 +8,14 @@ export default function CoursesNavigation() {
         className="list-group-item active border border-0">Home</Link>
       <Link id="wd-course-modules-link" to="/Kambaz/Courses/1234/Modules"
         className="list-group-item text-danger border border-0">Modules</Link>
-      <Link id="wd-course-piazza-link" to="/Kambaz/Courses/1234/Piazza"
-        className="list-group-item text-danger border border-0">Piazza</Link>
-      <Link id="wd-course-zoom-link" to="/Kambaz/Courses/1234/Zoom"
-        className="list-group-item text-danger border border-0">Zoom</Link>
+      <a id="wd-course-piazza-link" href="https://piazza.com/"
+        className="list-group-item text-danger border border-0"
+        target="_blank" 
+        rel="noopener noreferrer">Piazza</a>
+      <a id="wd-course-zoom-link" href="https://zoom.us/"
+        className="list-group-item text-danger border border-0"
+        target="_blank" 
+        rel="noopener noreferrer">Zoom</a>
       <Link id="wd-course-quizzes-link" to="/Kambaz/Courses/1234/Assignments"
         className="list-group-item text-danger border border-0">Assignments</Link>
       <Link id="wd-course-assignments-link" to="/Kambaz/Courses/1234/Quizzes"
