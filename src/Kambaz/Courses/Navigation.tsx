cb50dc2655b7eafe854dd3af3626718b444d0f0d
yatish79 <wutla.y@northeastern.edu>
2025-02-19 17:19:19 -0500

    nav

--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,29 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles.css";
-
 export default function CoursesNavigation() {
+  const { pathname } = useLocation();
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link id="wd-course-home-link" to="/Kambaz/Courses/1234/Home"
-        className="list-group-item active border border-0">Home</Link>
-      <Link id="wd-course-modules-link" to="/Kambaz/Courses/1234/Modules"
-        className="list-group-item text-danger border border-0">Modules</Link>
-      <a id="wd-course-piazza-link" href="https://piazza.com/"
-        className="list-group-item text-danger border border-0"
-        target="_blank" 
-        rel="noopener noreferrer">Piazza</a>
-      <a id="wd-course-zoom-link" href="https://zoom.us/"
-        className="list-group-item text-danger border border-0"
-        target="_blank" 
-        rel="noopener noreferrer">Zoom</a>
-      <Link id="wd-course-quizzes-link" to="/Kambaz/Courses/1234/Assignments"
-        className="list-group-item text-danger border border-0">Assignments</Link>
-      <Link id="wd-course-assignments-link" to="/Kambaz/Courses/1234/Quizzes"
-        className="list-group-item text-danger border border-0">Quizzes</Link>
-      <Link id="wd-course-grades-link" to="/Kambaz/Courses/1234/Grades"
-        className="list-group-item text-danger border border-0">Grades</Link>
-      <Link id="wd-course-people-link" to="/Kambaz/Courses/1234/People"
-        className="list-group-item text-danger border border-0">People</Link>
+      {links.map((link) => (
+        <Link to={`./${link}`}
+        className={`list-group-item border border-0 ${pathname.includes(link) ? "active" : "text-danger"}`}>
+          {link}
+        </Link>
+      ))}
     </div>
   );
 }
