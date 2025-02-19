3163a901a5010caf90568f5e8984b7b14c80d290
yatish79 <wutla.y@northeastern.edu>
2025-02-19 18:13:45 -0500

    c4

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -14,7 +14,7 @@ export default function Account() {
         <div className="flex-center">
           <Routes>
             <Route path="/"
-              element={<Navigate to="/Kanbas/Account/Signin" />} />
+              element={<Navigate to="/Kambaz/Account/Signin" />} />
             <Route path="/Signin" element={<Signin />} />
             <Route path="/Profile" element={<Profile />} />
             <Route path="/Signup" element={<Signup />} />
