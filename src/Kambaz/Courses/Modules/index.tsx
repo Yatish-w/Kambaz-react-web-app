07e4ffb660102498c2eafa5344f7dfcd52490e06
yatish79 <wutla.y@northeastern.edu>
2025-03-12 22:05:06 -0400

    i3

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -7,7 +7,7 @@ import { BsGripVertical } from "react-icons/bs";
 import ModuleControlButtons from "./ModuleControlButtons";
 import { useParams } from "react-router";
 //import * as db from "../../Database";
-import React, { useState } from "react";
+import  { useState } from "react";
 import { addModule, editModule, updateModule, deleteModule }
   from "./reducer";
 import { useSelector, useDispatch } from "react-redux";
