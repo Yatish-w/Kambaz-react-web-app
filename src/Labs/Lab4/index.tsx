bd09543e8fe58d235716d3921791dcc08fcf44e5
yatish79 <wutla.y@northeastern.edu>
2025-03-12 21:52:54 -0400

    imp1

--- src/Labs/Lab4/index.tsx
+++ src/Labs/Lab4/index.tsx
@@ -8,8 +8,8 @@ import ObjectStateVariable from "./ObjectStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
-import StringStateVariables from "./StringStateVariables";
 import ReduxExamples from "./ReduxExamples";
+import StringStateVariables from "./StringStateVariables";
 
 export default function Lab4() {
     function sayHello() {
