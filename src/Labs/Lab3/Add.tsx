e59e615ce2e18f760ec713a634d0635298a1ce01
yatish79 <wutla.y@northeastern.edu>
2025-04-02 22:58:08 -0400

    a5

--- src/Labs/Lab3/Add.tsx
+++ src/Labs/Lab3/Add.tsx
@@ -3,6 +3,7 @@ export default function Add({ a, b }: { a: number; b: number }) {
         <div id="wd-add">
             <h4>Add</h4>
             a = {a} b = {b} <br />
+            
             a + b = {a + b} <hr />
         </div>
     );
