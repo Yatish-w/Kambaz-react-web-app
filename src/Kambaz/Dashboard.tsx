1d32e0f805ce0f133bfe97853a2eea9e4ae52ad3
yatish79 <wutla.y@northeastern.edu>
2025-04-05 00:00:40 -0400

    Update session handling and authentication

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -35,7 +35,7 @@ export default function Dashboard(
     setShowAllCourses(!showAllCourses);
   };
 
-  const filteredCourses = currentUser.role === "FACULTY" || showAllCourses
+  const filteredCourses = showAllCourses
     ? courses
     : courses.filter((course) =>
       enrollments.some(
@@ -120,7 +120,7 @@ export default function Dashboard(
                   <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
                     {course.description} </p>
 
-                  {currentUser.role === "FACULTY" && (
+                  {currentUser.role === "FACULTY" && isEnrolled(course._id) && (
                     <>
                       <Link to={`/Kambaz/Courses/${course._id}/Home`}
                         className="wd-dashboard-course-link text-decoration-none text-dark" >
